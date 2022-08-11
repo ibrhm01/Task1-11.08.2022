213e19427929c71b07e2b6dc30e7d271bb7faf22
Ibrahim Aliyev <ibrahimma@code.edu.az>
2022-08-11 18:39:38 +0400

    Task1

---
+++ Task1-11.08.2022/Models/User.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1_11._08._2022.Models
+{
+    class User
+    {
+        private string _userName;
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                if (value.Length >= 6 && value.Length <= 25) _userName = value;
+            }
+        }
+        private string _password;
+        public string Password {
+            get
+            {
+                return _password;
+            }
+            set 
+            {
+                if (HasDigit(value) && HasUpper(value) && HasLower(value) && value.Length >= 8 && value.Length <=  25) _password = value;
+            }
+        }
+        public User(string Username)
+        {
+            this.UserName = Username;
+        }
+        public User(string Username, string Password):this(Username)
+        {
+            this.Password = Password;
+        }
+        public bool HasDigit(string value)
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (value[i] >= 48 && value[i] <= 57) return true;   
+            }
+            return false;
+
+        }
+        public bool HasUpper(string value)
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (value[i] >= 65 && value[i] <= 90) return true;
+            }
+            return false;
+
+        }
+        public bool HasLower(string value)
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (value[i] >= 97 && value[i] <= 122) return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Task1-11.08.2022/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Task1_11._08._2022.Models;
+
+namespace Task1_11._08._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User a = new User(Console.ReadLine(), Console.ReadLine());
+            
+            Console.WriteLine($"Username:{a.UserName}\nPassword:{a.Password}");
+        }
+    }
+}
